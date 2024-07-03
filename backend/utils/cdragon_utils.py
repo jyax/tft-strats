3549d2bd8a30b45bac562c09d53d7d31041a19ab
jyax <yaxjacob@msu.edu>
2024-07-03 12:50:33 -0400

    feat: Add CDragonFileBrowser class for fetching directories and checking files for keywords on CDragon's RAW file host

--- backend/utils/cdragon_utils.py
+++ backend/utils/cdragon_utils.py
@@ -15,3 +15,29 @@ def get_character_data():
     url = "https://raw.communitydragon.org/latest/game/data/tftteamplanner/characters.bin.json"
     response = requests.get(url=url)
     return response.json()
+
+
+class CDragonFileBrowser:
+    def __init__():
+        self.keywords = ["tft", "teamfighttactics", "team_fight_tactics", "teamfight_tactics"]
+    
+    
+    def fetch_directory(dir_url):
+        """
+        Fetches a directory from CDragon and returns the JSON tree
+        """
+        response = requests.get(dir_url)
+        response.raise_for_status()
+        return response.json()
+    
+    def check_file_for_keywords(self, file_url):
+        """
+        Checks a file for keywords
+        """
+        response = requests.get(file_url)
+        response.raise_for_status()
+        file_data = response.json()
+        for keyword in self.keywords:
+            if keyword in file_data:
+                return True
+        return False
